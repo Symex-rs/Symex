a1f79b76b09b4f1ad4aa9f7d65ca60d4af306899
ivajon <ivar.jonsson18@gmail.com>
2025-11-05 08:55:20 +0100

    Touchup of the docs

--- README.md
+++ README.md
@@ -1,71 +1,76 @@
-# SYMEX
+<h1 align="center">
+  SYMEX
+</h1>
 
-Symbolic execution engine that can operate on:
+Symex is a symbolic execution engine operating on compile binaries for one of the following targets at the users discretion:
 
-- LLVM IR
 - ARMv6-M
-- ARMv7-M/ARMv7-EM 
+- ARMv7-M/ARMv7-EM
 - RISC-V (only RV32I base integer instruction set is currently supported), for the [Hippomenes architecture](https://github.com/perlindgren/hippomenes).
 
- 
+The engine can provide cycle accurate results for the worst case execution time for applications compiled for `ARMv6-M` and `RISC-V` under certain conditions.
+As the engine operates on compiled binaries it it language agnostic at the core. However, the testing has been focused on Rust applications, more specifically [`RTIC-v1`](https://github.com/rtic-rs/rtic/tree/release/v1) applications.
 
-Main use is to analyze Rust programs but programs written in other languages can potentially be analyzed.
-Because the library used to read LLVM bytecode is large and cumbersome is the LLVM IR part of the tool hidden behind the feature flag `llvm`.
 
-Since Symex was originally written with only LLVM IR execution in mind are the integration of machine code execution not always done coherently.
-Work is ongoing on how the two parts should coexist.
-Because of this is the following documentation split up in two different parts one for LLVM IR and one for ARMv6-M/ARMv7-M/ARMv7-EM/RISC-V machine code.
-
-## Binary
-
-### Getting started
+# Getting started
 
 The easiest way to use Symex is by the cargo-symex tool.
-It can be installed by running:
+To install it you must first install the SMT solver of choice, by default this is [bitwuzla](https://github.com/bitwuzla/bitwuzla), typically this can be installed from your system package manager.
+Once the smt-solver has been installed, one can install cargo-symex by running:
 
-```plain
+```bash
 cargo install --path cargo-symex
 ```
 
-Then the examples can be executed by first navigating to the `armv6-m-examples` directory and executing:
+This can then be used to compile examples or binaries and executing the (mangled) name of the function like so:
 
-```plain
-cargo symex --elf --example [example name] --function [function name] (--release)
+```bash
+cargo symex --example [example name] --function [function name] (--release) # for examples
+cargo symex --bin [example name] --function [function name] (--release) # for binaries
 ```
 
-### Additional notes
+Or it can be used to run a pre-compiled binary by calling it like so
+```bash
+cargo symex --path [path to .elf file] --function [function name] (--release)
+```
+
+
+## Application notes
+
+Analysis of compiled binaries has a few caveats, namely:
 
 - To analyze a function it must have an entry in the `.symtab` section of the elf file. All symbols in an elf file can be shown using the `readelf -s [path to elf file]` command. To tell rustc to not mangle the function name the attribute `#[no_mangle]` can be used.
 - When using symex-lib functions or to be able to detect panic the debug-data must be included in the elf file.
-  An elf file can directly be analyzed with cargo-symex by the `cargo symex --elf --path [path to elf file] --function [function name]`
 - Symex can be directly used as a library see `wcet-analasis-example` directory for examples of how to do that.
 
-### Notes on the max cycle count on armv6-m
+## Notes on the max cycle count on ARMv6-M
 
 The max cycle count for each path is calculated by counting the number of cycles for each instruction according to [this document](https://developer.arm.com/documentation/ddi0432/c/programmers-model/instruction-set-summary). It assumes a core without wait-states.
 
-### Notes on cycle counting for armv7-(e)m
+## Notes on cycle counting for ARMv7-(E)M
 
 The cycle counting model does not contain any model of a branch predictor. This means that the branching model always flushes the pipeline thus incurring a lot more cycles estimated as soon as a branch can be predicted by the hardware.
 This could be improved greatly by adding a branch prediction model. The cycle counting model also assumes that the code encounters zero wait states, i.e. the code is running from RAM. The Cycle counts for each instruction are based on the [cortex-m4 documentation](https://developer.arm.com/documentation/ddi0439/b/CHDDIGAC).
 
-### Note on cycle counting for RISC-V
+## Note on cycle counting for RISC-V
 
 The cycle counts are based on the single-cycle, non-pipelined [Hippomenes architecture](https://github.com/perlindgren/hippomenes).
 
-### Limitations for armv7-(e)m
+### Limitations for ARMv7-(E)M
 
-The armv7 support lacks implementations for [`DSP`](https://developer.arm.com/documentation/ddi0403/d/Application-Level-Architecture/The-ARMv7-M-Instruction-Set/Data-processing-instructions/Parallel-addition-and-subtraction-instructions--DSP-extension) and the [`floating point extension`](https://developer.arm.com/documentation/ddi0403/d/Application-Level-Architecture/Application-Level-Programmers--Model/The-optional-Floating-point-extension). The DSP extension is parsable by the [`disarmv7`](https://github.com/ivario123/disarmv7) but is not implemented in the [decoder](symex/src/general_assembly/arch/arm/v7/decoder.rs).
-Armv7 has support for hardware semaphores, at the time of writing these are not implemented in symex.
+The ARMv7 support has partial implementations for [`DSP`](https://developer.arm.com/documentation/ddi0403/d/Application-Level-Architecture/The-ARMv7-M-Instruction-Set/Data-processing-instructions/Parallel-addition-and-subtraction-instructions--DSP-extension) and the [`floating point extension`](https://developer.arm.com/documentation/ddi0403/d/Application-Level-Architecture/Application-Level-Programmers--Model/The-optional-Floating-point-extension). The DSP extension is parsable by the [`disarmv7`](https://github.com/ivario123/disarmv7) but is not implemented in the [decoder](symex/src/general_assembly/arch/arm/v7/decoder.rs).
+Armv7 has support for hardware semaphores, at the time of writing these are not implemented in Symex.
+Finally, the ARMv7 ISA defines floating point operations, most of these are partially supported. However, as outlined in 2
+
 
 ### Future work planned or unplanned
 
 #### Improve testing suite
 
-The assembly to symex GA translators needs further testing.
+The assembly to Symex GA translators needs further testing.
 
-- The armv7 translator is only $\approx 26\%$ tested, due to the nature of this project we should strive to improve this.
-- The armv6 translator lacks direct testing and should also be improved.
+- The ARMv7 translator is only $\approx 26\%$ tested, due to the nature of this project we should strive to improve this.
+- The ARMv6 translator lacks direct testing and should also be improved.
 
 <details>
   <summary>Test percentage</summary>
@@ -79,115 +84,22 @@ it shows that more testing is needed.
 
 The current (v7) implementation lacks support for [DSP](#limitations-for-armv7-em) instructions, most of these can be implemented without large changes.
 
-#### Include [`floating point extension`](https://developer.arm.com/documentation/ddi0403/d/Application-Level-Architecture/Application-Level-Programmers--Model/The-optional-Floating-point-extension)
-
-The current (v7) implementation lacks support for floating point instructions, these are also missing from the [`disarmv7`](https://github.com/ivario123/disarmv7) crate so implementing support for floating point is a more extensive change.
-
 #### Include support for hardware semaphores
 
 This is nontrivial as it extensive modeling of the system if it is to be useful. However, we could implement the baseline definition from the data sheet if we simply added a hashmap to keep track of which memory addresses are subject to a semaphore.
 
-#### Support for more RISC-V instruction sets
+#### Support more RISC-V instruction sets
 
 Currently limited to RV32I base integer instruction set ([Hippomenes architecture](https://github.com/perlindgren/hippomenes)).
 
-## LLVM IR
-
-### Cargo subcommand
-
-A cargo subcommand is available to easily compile Rust programs into bitcode files and run them
-in the symbolic execution engine.
-
-It can be installed with
-
-```shell
-> cargo install --path cargo-symex --features llvm
-```
-
-For usage instructions see `cargo symex --help`.
-
-### Getting started
-
-Check out the examples contained in `examples/examples`. These can be run with the cargo subcommand
-
-```shell
-> cd examples
-> cargo-symex --example <example>
-```
-
-To compile and run the example `examples/rust_simple` using the cargo subcommand
-
-```shell
-> cd examples
-> cargo symex --example simple --function rust_simple_test
-```
-
-This will display the results of the analysis of the example, showing all the paths it took and
-concrete values for all inputs and output.
-
-### Building
+## Building
 
 #### Dependencies
 
-- [LLVM](https://llvm.org/), used as a library for decoding the LLVM-IR (internal representation)
-  of the program under analysis.
-- [boolector](https://github.com/Boolector/boolector), Boolector is a Satisfiability Modulo Theories
+- [bitwuzla](https://github.com/bitwuzla/bitwuzla), Bitwuzla is a Satisfiability Modulo Theories
   (SMT) solver for the theories of fixed-size bit-vectors, arrays and uninterpreted functions.
-
-The project currently uses LLVM 17 which require a relatively recent version of Rust.
-
-#### Devcontainer
-
-As an alternative, a [Dev Container](https://code.visualstudio.com/docs/devcontainers/containers) is provided that automatically installs Rust `1.72` and LLVM 17.
-
-To generate tests use `./symex/compile_tests_dc.sh` instead.
-
-### Making tests work
-
-The tests make use of compiled LLVM IR files which are not tracked by git. To make the tests work
-run
-
-```shell
-> ./scripts/compile_tests.sh
-```
-
-### Known issues
-
-Sometimes when running examples the runner may give
-
-```shell
-error: could not copy "<project_dir>/target/debug/examples/<some_file>.bc" to "<project_dir>/target/debug/examples/<some_file>.bc": No such file or directory (os error 2)
-
-error: could not copy "<project_dir>/target/debug/examples/<some_file>.ll" to "<project_dir>/target/debug/examples/<some_file>.ll": No such file or directory (os error 2)
-
-error: could not compile `examples` due to 2 previous errors
-```
-
-Until the issue is fixed it can remedied by running `cargo clean` and trying again.
-
-## SYMEX LLVM 14
-
-The older version `symex-llvm-14-legacy` is still available (for now). But it will be phased out later in favor of the more updated version that supports LLVM 17.
-
-Due to LLVM dependencies, the current implementation is limited to Rust < 1.64. `cd` to the folder where you want to run `cargo symex` from and override the Rust version to be used as below:
-
-```shell
-cd <folder>
-rustup override set 1.64
-```
-
-Required dependencies for `symex-llvm-14-legacy`
-
-- [LLVM](https://llvm.org/), used as a library for decoding the LLVM-IR (internal representation)
-  of the program under analysis.
-
-SMT solver defaults to `boolector`. It is possible to use Z3 instead of Boolector by using the feature flag `z3`.
-
-- [boolector](https://github.com/Boolector/boolector), Boolector is a Satisfiability Modulo Theories
+- (Optional) [boolector](https://github.com/Boolector/boolector), Boolector is a Satisfiability Modulo Theories
   (SMT) solver for the theories of fixed-size bit-vectors, arrays and uninterpreted functions.
-- [Z3](https://github.com/Z3Prover/z3), Z3 is a theorem prover from Microsoft Research.
-
-To use the devcontainer with this setup, see the notes in `.devcontainer/devcontainer.json`.
 
 ## Debug output from SYMEX
 
@@ -214,8 +126,6 @@ You can also narrow down the scope to specific modules, e.g. the executor.
 ```shell
 > RUST_LOG="symex::executor=trace" cargo symex ...
 ```
-
-## Future work
 
 ## License
 
--- rust-debug/src/variable.rs
+++ rust-debug/src/variable.rs
@@ -1,17 +1,23 @@
+use anyhow::{anyhow, Result};
 use gimli::{
     AttributeValue::{DebugInfoRef, DebugStrRef, Exprloc, LocationListsRef, UnitRef},
-    DebuggingInformationEntry, Dwarf, Reader, Unit, UnitOffset, UnitSectionOffset,
+    DebuggingInformationEntry,
+    Dwarf,
+    Reader,
+    Unit,
+    UnitOffset,
+    UnitSectionOffset,
 };
-
-use crate::evaluate::attributes;
-use crate::evaluate::evaluate;
-use crate::registers::Registers;
-use crate::source_information::SourceInformation;
-use crate::utils::in_range;
-use crate::variable::evaluate::EvaluatorValue;
-use crate::{call_stack::MemoryAccess, utils::DwarfOffset};
-use anyhow::{anyhow, Result};
 use log::{error, info, trace};
+
+use crate::{
+    call_stack::MemoryAccess,
+    evaluate::{attributes, evaluate},
+    registers::Registers,
+    source_information::SourceInformation,
+    utils::{in_range, DwarfOffset},
+    variable::evaluate::EvaluatorValue,
+};
 
 /// Defines what debug information a variable has.
 #[derive(Debug, Clone)]
@@ -19,7 +25,8 @@ pub struct Variable<R: Reader<Offset = usize>> {
     /// The name of the variable.
     pub name: Option<String>,
 
-    /// A tree structured like the variable type in DWARF, but it also contains the values
+    /// A tree structured like the variable type in DWARF, but it also contains
+    /// the values
     pub value: EvaluatorValue<R>,
 
     /// The source code location where the variable was declared.
@@ -33,17 +40,21 @@ impl<R: Reader<Offset = usize>> Variable<R> {
     ///
     /// * `dwarf` - A reference to gimli-rs `Dwarf` struct.
     /// * `registers` - A reference to the `Registers` struct.
-    /// * `memory` - A reference to a struct that implements the `MemoryAccess` trait.
-    /// * `section_offset` - A offset to the compilation unit where the DIE for the variable is
+    /// * `memory` - A reference to a struct that implements the `MemoryAccess`
+    ///   trait.
+    /// * `section_offset` - A offset to the compilation unit where the DIE for
+    ///   the variable is
     /// located.
-    /// * `unit_offset` - A offset into the compilation unit where the DIE representing the
+    /// * `unit_offset` - A offset into the compilation unit where the DIE
+    ///   representing the
     /// variable is located.
-    /// * `frame_base` - The value of the frame base, which is often needed to evaluate the
+    /// * `frame_base` - The value of the frame base, which is often needed to
+    ///   evaluate the
     /// variable.
     /// * `cwd` - The work directory of the program being debugged.
     ///
-    /// This function will go through the DIE in the compilation unit to find the necessary
-    /// debug information.
+    /// This function will go through the DIE in the compilation unit to find
+    /// the necessary debug information.
     /// Then it will use that information to evaluate the value of the variable.
     pub fn get_variable<M: MemoryAccess>(
         dwarf: &Dwarf<R>,
@@ -58,23 +69,18 @@ impl<R: Reader<Offset = usize>> Variable<R> {
             .get_register_value(
                 &(registers
                     .program_counter_register
-                    .ok_or_else(|| anyhow!("Requires that the program counter register is known"))?
-                    as u16),
+                    .ok_or_else(|| anyhow!("Requires that the program counter register is known"))? as u16),
             )
             .ok_or_else(|| anyhow!("Requires that the program counter registers has a value"))?;
 
         // Get the variable die.
-        let header = dwarf.debug_info.header_from_offset(
-            match dwarf_offset.section_offset.as_debug_info_offset() {
-                Some(val) => val,
-                None => {
-                    error!("Could not convert section offset into debug info offset");
-                    return Err(anyhow!(
-                        "Could not convert section offset into debug info offset"
-                    ));
-                }
-            },
-        )?;
+        let header = dwarf.debug_info.header_from_offset(match dwarf_offset.section_offset.as_debug_info_offset() {
+            Some(val) => val,
+            None => {
+                error!("Could not convert section offset into debug info offset");
+                return Err(anyhow!("Could not convert section offset into debug info offset"));
+            }
+        })?;
         let unit = gimli::Unit::new(dwarf, header)?;
         let die = unit.entry(dwarf_offset.unit_offset)?;
 
@@ -119,43 +125,26 @@ impl<R: Reader<Offset = usize>> Variable<R> {
         let (type_section_offset, type_unit_offset) = find_variable_type_die(dwarf, &unit, &die)?;
         info!("type sec offset: {:?}", type_section_offset);
         info!("type unit offset: {:?}", type_unit_offset);
-        let header = dwarf.debug_info.header_from_offset(
-            match type_section_offset.as_debug_info_offset() {
-                Some(val) => val,
-                None => {
-                    error!("Could not convert section offset into debug info offset");
-                    return Err(anyhow!(
-                        "Could not convert section offset into debug info offset"
-                    ));
-                }
-            },
-        )?;
+        let header = dwarf.debug_info.header_from_offset(match type_section_offset.as_debug_info_offset() {
+            Some(val) => val,
+            None => {
+                error!("Could not convert section offset into debug info offset");
+                return Err(anyhow!("Could not convert section offset into debug info offset"));
+            }
+        })?;
         let type_unit = gimli::Unit::new(dwarf, header)?;
         let type_die = type_unit.entry(type_unit_offset)?;
 
         info!("has type");
 
-        let value = evaluate(
-            dwarf,
-            &unit,
-            pc,
-            expression,
-            frame_base,
-            Some(&type_unit),
-            Some(&type_die),
-            registers,
-            memory,
-        )?;
+        let value = evaluate(dwarf, &unit, pc, expression, frame_base, Some(&type_unit), Some(&type_die), registers, memory)?;
 
-        Ok(Variable {
-            name,
-            value,
-            source,
-        })
+        Ok(Variable { name, value, source })
     }
 }
 
-/// Will check if the given DIE has one of the DWARF tags that represents a variable.
+/// Will check if the given DIE has one of the DWARF tags that represents a
+/// variable.
 ///
 /// Description:
 ///
@@ -165,12 +154,11 @@ impl<R: Reader<Offset = usize>> Variable<R> {
 /// - DW_TAG_variable
 /// - DW_TAG_formal_parameter
 /// - DW_TAG_constant
-/// If the DIE has one of the tags the function will return `true`, otherwise `false`.
+/// If the DIE has one of the tags the function will return `true`, otherwise
+/// `false`.
 pub fn is_variable_die<R: Reader<Offset = usize>>(die: &DebuggingInformationEntry<R>) -> bool {
     // Check that it is a variable.
-    die.tag() == gimli::DW_TAG_variable
-        || die.tag() == gimli::DW_TAG_formal_parameter
-        || die.tag() == gimli::DW_TAG_constant
+    die.tag() == gimli::DW_TAG_variable || die.tag() == gimli::DW_TAG_formal_parameter || die.tag() == gimli::DW_TAG_constant
 }
 
 /// Will retrieve the name of a variable DIE.
@@ -178,20 +166,17 @@ pub fn is_variable_die<R: Reader<Offset = usize>>(die: &DebuggingInformationEntr
 /// Description:
 ///
 /// * `dwarf` - A reference to gimli-rs `Dwarf` struct.
-/// * `unit` - A reference to gimli-rs `Unit` struct, which the given DIE is located in.
+/// * `unit` - A reference to gimli-rs `Unit` struct, which the given DIE is
+///   located in.
 /// * `die` - A reference to DIE.
 ///
-/// Will check if the given DIE represents a variable, if it does not it will return a error.
-/// After that it will try to evaluate the `DW_AT_name` attribute and return the result.
-/// But if it dose not have the name attribute it will try to get the name from the DIE in the
-/// `DW_AT_abstract_origin` attribute.
-/// If that attribute is missing it will return `Ok(None)`, because the variable does not have a
-/// name.
-pub fn get_var_name<R: Reader<Offset = usize>>(
-    dwarf: &Dwarf<R>,
-    unit: &Unit<R>,
-    die: &DebuggingInformationEntry<R>,
-) -> Result<Option<String>> {
+/// Will check if the given DIE represents a variable, if it does not it will
+/// return a error. After that it will try to evaluate the `DW_AT_name`
+/// attribute and return the result. But if it dose not have the name attribute
+/// it will try to get the name from the DIE in the `DW_AT_abstract_origin`
+/// attribute. If that attribute is missing it will return `Ok(None)`, because
+/// the variable does not have a name.
+pub fn get_var_name<R: Reader<Offset = usize>>(dwarf: &Dwarf<R>, unit: &Unit<R>, die: &DebuggingInformationEntry<R>) -> Result<Option<String>> {
     if is_variable_die(die) {
         // Get the name of the variable.
         if let Ok(Some(DebugStrRef(offset))) = die.attr_value(gimli::DW_AT_name) {
@@ -234,11 +219,12 @@ pub enum VariableLocation<R: Reader<Offset = usize>> {
     /// The gimli-rs expression that describes the location of the variable.
     Expression(gimli::Expression<R>),
 
-    /// The gimli-rs location list entry that describes the location of the Variable.
+    /// The gimli-rs location list entry that describes the location of the
+    /// Variable.
     LocationListEntry(gimli::LocationListEntry<R>),
 
-    /// The variable has no location currently but had or will have one. Note that the location can
-    /// be a constant stored in the DWARF stack.
+    /// The variable has no location currently but had or will have one. Note
+    /// that the location can be a constant stored in the DWARF stack.
     LocationOutOfRange,
 
     /// The variable has no location.
@@ -250,17 +236,15 @@ pub enum VariableLocation<R: Reader<Offset = usize>> {
 /// Description:
 ///
 /// * `dwarf` - A reference to gimli-rs `Dwarf` struct.
-/// * `unit` - A reference to gimli-rs `Unit` struct which contains the given DIE.
+/// * `unit` - A reference to gimli-rs `Unit` struct which contains the given
+///   DIE.
 /// * `die` - A reference to the variables DIE that contains the location.
-/// * `address` - A address that will be used to find the location, this is most often the current machine code address.
+/// * `address` - A address that will be used to find the location, this is most
+///   often the current machine code address.
 ///
-/// Will get the location for the given address from the attribute `DW_AT_location` in the variable DIE.
-pub fn find_variable_location<R: Reader<Offset = usize>>(
-    dwarf: &Dwarf<R>,
-    unit: &Unit<R>,
-    die: &DebuggingInformationEntry<R>,
-    address: u32,
-) -> Result<VariableLocation<R>> {
+/// Will get the location for the given address from the attribute
+/// `DW_AT_location` in the variable DIE.
+pub fn find_variable_location<R: Reader<Offset = usize>>(dwarf: &Dwarf<R>, unit: &Unit<R>, die: &DebuggingInformationEntry<R>, address: u32) -> Result<VariableLocation<R>> {
     if is_variable_die(die) {
         match die.attr_value(gimli::DW_AT_location)? {
             Some(Exprloc(expr)) => Ok(VariableLocation::Expression(expr)),
@@ -296,17 +280,15 @@ pub fn find_variable_location<R: Reader<Offset = usize>>(
 /// Description:
 ///
 /// * `dwarf` - A reference to gimli-rs `Dwarf` struct.
-/// * `unit` - A reference to gimli-rs `Unit` struct, which the given DIE is located in.
-/// * `die` - A reference to the DIE representing a variable, which the resulting type DIE will represent the type off..
+/// * `unit` - A reference to gimli-rs `Unit` struct, which the given DIE is
+///   located in.
+/// * `die` - A reference to the DIE representing a variable, which the
+///   resulting type DIE will represent the type off..
 ///
-/// Will find the DIE representing the type of the variable that the given DIE represents.
-/// The type DIE is found using the attribute `DW_AT_type` in the given DIE or in the DIE from the
-/// attribute `DW_AT_abstract_origin`.
-pub fn find_variable_type_die<R: Reader<Offset = usize>>(
-    dwarf: &Dwarf<R>,
-    unit: &Unit<R>,
-    die: &DebuggingInformationEntry<R>,
-) -> Result<(UnitSectionOffset, UnitOffset)> {
+/// Will find the DIE representing the type of the variable that the given DIE
+/// represents. The type DIE is found using the attribute `DW_AT_type` in the
+/// given DIE or in the DIE from the attribute `DW_AT_abstract_origin`.
+pub fn find_variable_type_die<R: Reader<Offset = usize>>(dwarf: &Dwarf<R>, unit: &Unit<R>, die: &DebuggingInformationEntry<R>) -> Result<(UnitSectionOffset, UnitOffset)> {
     if is_variable_die(die) {
         match attributes::type_attribute(dwarf, unit, die)? {
             Some(result) => Ok(result),
@@ -351,19 +333,16 @@ pub fn find_variable_type_die<R: Reader<Offset = usize>>(
 /// Description:
 ///
 /// * `dwarf` - A reference to gimli-rs `Dwarf` struct.
-/// * `unit` - A reference to gimli-rs `Unit` struct, which the given DIE is located in.
+/// * `unit` - A reference to gimli-rs `Unit` struct, which the given DIE is
+///   located in.
 /// * `die` - A reference to DIE.
 /// * `cwd` - The work directory of the debugged program.
 ///
-/// This function will retrieve the source code location where the variable was declared.
-/// The information is retrieved from the  attributes starting with `DW_AT_decl_` in the given DIE,
-/// or in the DIE found in the attribute `DW_AT_abstract_origin`.
-pub fn find_variable_source_information<R: Reader<Offset = usize>>(
-    dwarf: &Dwarf<R>,
-    unit: &Unit<R>,
-    die: &DebuggingInformationEntry<R>,
-    cwd: &str,
-) -> Result<SourceInformation> {
+/// This function will retrieve the source code location where the variable was
+/// declared. The information is retrieved from the  attributes starting with
+/// `DW_AT_decl_` in the given DIE, or in the DIE found in the attribute
+/// `DW_AT_abstract_origin`.
+pub fn find_variable_source_information<R: Reader<Offset = usize>>(dwarf: &Dwarf<R>, unit: &Unit<R>, die: &DebuggingInformationEntry<R>, cwd: &str) -> Result<SourceInformation> {
     if is_variable_die(die) {
         if let Ok(Some(die_offset)) = die.attr_value(gimli::DW_AT_abstract_origin) {
             match die_offset {
